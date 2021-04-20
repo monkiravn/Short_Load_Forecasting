8e03a0cd1cd00182dceeab0b828e0a7470eba217
Tien Nguyen <monkiradgbn01@gmail.com>
2021-04-20 10:56:10 +0700

    First commit

---
+++ data_formatters/electricload.py
@@ -0,0 +1,39 @@
+import data_formatters.base
+import libs.utils as utils
+import sklearn.preprocessing
+
+GenericDataFormatter = data_formatters.base.GenericDataFormatter
+DataTypes = data_formatters.base.DataTypes
+InputTypes = data_formatters.base.InputTypes
+
+class ElectricityFormatter(GenericDataFormatter):
+  """Defines and formats data for the electricity dataset.
+
+  Note that per-entity z-score normalization is used here, and is implemented
+  across functions.
+
+  Attributes:
+    column_definition: Defines input and data type of column used in the
+      experiment.
+    identifiers: Entity identifiers used in experiments.
+  """
+
+  _column_definition = [
+      ('id', DataTypes.REAL_VALUED, InputTypes.ID),
+      ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.TIME),
+      ('power_usage', DataTypes.REAL_VALUED, InputTypes.TARGET),
+      ('hour', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+      ('day_of_week', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+      ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+      ('categorical_id', DataTypes.CATEGORICAL, InputTypes.STATIC_INPUT),
+  ]
+
+  def __init__(self):
+    """Initialises formatter."""
+
+    self.identifiers = None
+    self._real_scalers = None
+    self._cat_scalers = None
+    self._target_scaler = None
+    self._num_classes_per_cat_input = None
+    self._time_steps = self.get_fixed_params()['total_time_steps']
